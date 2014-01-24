0a65d032c61b3ae55d06ab474585cdf275f2ebf2
James Halliday <mail@substack.net>
2014-01-24 14:25:50 -0800

    output hooked into browser-pack

--- bin/cmd.js
+++ bin/cmd.js
@@ -1,13 +1,17 @@
 #!/usr/bin/env node
 
+var fs = require('fs');
+var path = require('path');
 var factor = require('../');
 var minimist = require('minimist');
 var pack = require('browser-pack');
-var argv = minimist(process.argv.slice(2));
 
-var fr = factor(argv._);
+var argv = minimist(process.argv.slice(2));
+var basedir = argv.basedir || process.cwd();
+
+var fr = factor(argv._, { raw: true, root: basedir });
 var files = argv._.reduce(function (acc, x, ix) {
-    acc[x] = argv._[ix];
+    acc[path.resolve(basedir, argv._[ix])] = argv.o[ix];
     return acc;
 }, {});
 
@@ -15,4 +19,4 @@ fr.on('stream', function (bundle) {
     var ws = fs.createWriteStream(files[bundle.file]);
     bundle.pipe(pack({ raw: true })).pipe(ws);
 });
-process.stdin.pipe(fr).pipe(process.stdout);
+process.stdin.pipe(fr).pipe(pack({ raw: true })).pipe(process.stdout);
