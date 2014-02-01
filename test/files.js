5a74ef5de29d6b13dfab8a90096e88b1bfa3f47e
Eric Drechsel <eric@pdxhub.org>
2014-01-31 17:59:35 -0800

    test for threshold function (in|ex)cluding files from common (for #6)

--- test/files.js
+++ test/files.js
@@ -149,6 +149,96 @@ test('same module included twice', function (t) {
     fr.pipe(packs.common);
 });
 
+test('threshold function reorganizes bundles', function (t) {
+    t.plan(5);
+    var files = [ 'y.js', 't.js' ].map(function (file) {
+      return path.join(__dirname, 'files', file);
+    });
+
+    var expected = {
+        common: [
+            read('z.js')
+        ],
+        't.js': [
+            read('t.js', {
+                entry: true,
+                deps: { './a.js': norm('a.js'), './w.js': norm('w.js') }
+            }),
+            read('a.js'),
+            read('w.js', {
+                deps: { './a.js': norm('a.js') }
+            })
+        ],
+        'y.js': [
+            read('y.js', {
+                entry: true,
+                deps: { './z.js': norm('z.js'), './a.js': norm('a.js') }
+            }),
+            read('a.js')
+        ]
+    };
+
+    var packs = {
+        common: pack({ raw: true }),
+        't.js': pack({ raw: true }),
+        'y.js': pack({ raw: true })
+    };
+
+    var pending = 3;
+
+    var sources = {};
+    packs.common.pipe(concat(function (src) {
+        sources.common = src;
+        done();
+    }));
+    packs['t.js'].pipe(concat(function (src) {
+        sources['t.js'] = src;
+        done();
+    }));
+    packs['y.js'].pipe(concat(function (src) {
+        sources['y.js'] = src;
+        done();
+    }));
+
+    function done () {
+        if (--pending !== 0) return;
+        var srct = 'require=' + sources.common
+            + ';require=' + sources['t.js']
+        ;
+        function logt (msg) { t.equal(msg, 300) }
+        vm.runInNewContext(srct, { console: { log: logt } });
+
+        var srcy = 'require=' + sources.common
+            + ';require=' + sources['y.js']
+        ;
+        function logy (msg) { t.equal(msg, 333) }
+        vm.runInNewContext(srcy, { console: { log: logy } });
+    }
+
+    var rows = [];
+    var fr = factor(files, { objectMode: true, raw: true, threshold: function(row, groups) {
+        if (/.*a\.js$/.test(row.id)) {
+            return false;
+        };
+        if (/.*[z]\.js$/.test(row.id)) {
+            return true;
+        };
+        return this._defaultThreshold(row, groups);
+    }});
+    fr.on('stream', function (bundle) {
+        var name = path.basename(bundle.file);
+        bundle.pipe(rowsOf(function (rows) {
+            t.deepEqual(rows, expected[name]);
+        }));
+        bundle.pipe(packs[name]);
+    });
+    mdeps(files).pipe(fr)
+    fr.pipe(rowsOf(function (rows) {
+        t.deepEqual(rows, expected.common);
+    }));
+    fr.pipe(packs.common);
+});
+
 function rowsOf (cb) {
     var rows = [];
     return through(write, end);
