f648ecb2b1d659e9a1f4a384777b3800d2c0b6d3
John Gozde <john@gozde.ca>
2014-08-28 22:03:47 -0600

    Add regression test for dedupe dependencies.

---
+++ test/dedupe/c.js
@@ -0,0 +1,3 @@
+var d = require('./d');
+var x = require('./x');
+console.log(d(2) * x(3) * 11);
---
+++ test/plugin-dedupe.js
@@ -0,0 +1,47 @@
+var test = require('tape');
+var os = require('os');
+var fs = require('fs');
+var vm = require('vm');
+var mkdirp = require('mkdirp');
+var spawn = require('child_process').spawn;
+var browserify = require('browserify');
+var factor = require('../');
+var concat = require('concat-stream');
+
+var files = [
+    __dirname + '/dedupe/a.js',
+    __dirname + '/dedupe/b.js',
+    __dirname + '/dedupe/c.js'
+];
+var tmpdir = os.tmpdir() + '/factor-bundle-' + Math.random();
+mkdirp.sync(tmpdir);
+
+test('browserify plugin handles deduped modules', function (t) {
+    t.plan(4);
+    var args = files.concat('-p', '[',
+        __dirname + '/..',
+        '-o', tmpdir + '/a.js', '-o', tmpdir + '/b.js', '-o', tmpdir + '/c.js',
+        ']', '-o', tmpdir + '/common.js'
+    );
+    var ps = spawn('browserify', args);
+    ps.on('exit', function (code) {
+        t.equal(code, 0);
+
+        var common = fs.readFileSync(tmpdir + '/common.js', 'utf8');
+        var a = fs.readFileSync(tmpdir + '/a.js', 'utf8');
+        var b = fs.readFileSync(tmpdir + '/b.js', 'utf8');
+        var c = fs.readFileSync(tmpdir + '/c.js', 'utf8');
+
+        vm.runInNewContext(common + a, { console: { log: function (msg) {
+            t.equal(msg, 7770);
+        } } });
+
+        vm.runInNewContext(common + b, { console: { log: function (msg) {
+            t.equal(msg, 1155);
+        } } });
+
+        vm.runInNewContext(common + c, { console: { log: function (msg) {
+            t.equal(msg, 11550);
+        } } });
+    });
+});
