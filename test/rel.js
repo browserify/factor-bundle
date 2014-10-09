a476335c43ade461b403e360f05800da62914d1d
James Halliday <mail@substack.net>
2014-10-09 08:16:55 +0800

    failing relative input file test

---
+++ test/rel.js
@@ -0,0 +1,62 @@
+var test = require('tape');
+var os = require('os');
+var fs = require('fs');
+var path = require('path');
+var vm = require('vm');
+var mkdirp = require('mkdirp');
+var browserify = require('browserify');
+var factor = require('../');
+var concat = require('concat-stream');
+
+var tmpdir = os.tmpdir() + '/factor-bundle-' + Math.random();
+mkdirp.sync(tmpdir);
+
+var cwd = process.cwd();
+process.chdir(__dirname);
+
+var files = [ './rel/x.js', './rel/y.js' ];
+var outputs = [
+    path.join(tmpdir, 'x.js'),
+    path.join(tmpdir, 'y.js')
+];
+
+test('relative entry paths', function (t) {
+    t.plan(2);
+    t.on('end', function () { process.chdir(cwd) });
+    
+    var sources = {};
+    var pending = 3;
+    var b = browserify(files);
+    
+    b.plugin(factor, {
+        o: [
+            concat(function(data) {
+                sources.x = data.toString('utf8');
+                done();
+            }),
+            concat(function(data) {
+                sources.y = data.toString('utf8');
+                done();
+            })
+        ]
+    });
+
+    b.bundle().pipe(concat(function (data) {
+        sources.common = data.toString('utf8');
+        done();
+    }));
+    
+    function done () {
+        if (--pending !== 0) return;
+        var x = sources.common + sources.x;
+        var y = sources.common + sources.y;
+        
+        vm.runInNewContext(x, { console: { log: function (msg) {
+            t.equal(msg, 55500);
+        } } });
+        
+        vm.runInNewContext(y, { console: { log: function (msg) {
+            t.equal(msg, 333);
+        } } });
+    }
+});
