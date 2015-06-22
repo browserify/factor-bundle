f0bdca714543cc6be10192cfe1a3f9f3af8dcb9e
zoubin <zoubin04@gmail.com>
2015-06-22 16:12:43 +0800

    support callback outputs

--- test/outputs.js
+++ test/outputs.js
@@ -68,3 +68,32 @@ test('stream outputs', function (t) {
         } } });
     }
 });
+
+test('callback outputs', function (t) {
+    t.plan(2);
+    var sources = {}, pending = 3;
+    function write (key) {
+        return concat(function (body) {
+            sources[key] = body.toString('utf8');
+            if (-- pending === 0) done();
+        });
+    }
+
+    var b = browserify(files);
+    b.plugin(factor, { outputs: function () {
+        return [ write('x'), write('y') ];
+    }});
+    b.bundle().pipe(write('common'));
+
+    function done () {
+        var common = sources.common, x = sources.x, y = sources.y;
+
+        vm.runInNewContext(common + x, { console: { log: function (msg) {
+            t.equal(msg, 55500);
+        } } });
+
+        vm.runInNewContext(common + y, { console: { log: function (msg) {
+            t.equal(msg, 333);
+        } } });
+    }
+});
